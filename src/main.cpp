1802fe8258fea183cd2a6bf6bd87c1d77f9678e6
Blackseven <2662269845@qq.com>
2023-07-19 11:15:54 +0800

    The first upload

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <glm/glm.hpp>
+#include <tinynurbs/tinynurbs.h>
+#include <emmintrin.h>
+
+using namespace std;
+
+int main(){
+
+    cout<<"hi"<<endl;
+    return 0;
+
+}
